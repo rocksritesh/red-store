bda17c02a354dccfffb8e173da265fbb6215a07e
rocksritesh <webritesh1400@gmail.com>
2024-07-22 17:59:03 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+var MenuItems = document.getElementById('MenuItems');
+MenuItems.style.maxHeight = '0px';
+
+function menutoggle() {
+  if (MenuItems.style.maxHeight == '0px') {
+    MenuItems.style.maxHeight = '200px';
+  } else {
+    MenuItems.style.maxHeight = '0px';
+  }
+}
+
+let search = document.querySelector('.search-box');
+
+document.querySelector('#search-icon').onclick =() => {
+    search.classList.toggle('active');
+}
